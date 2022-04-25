e49c614d1c6f67f4e8ad2e595e850b4645da0795
Joao Ormeño <joao23.1996@gmail.com>
2022-04-25 19:19:00 -0400

    modificacion pagina | avances en sendfile para url

--- app.js
+++ app.js
@@ -9,21 +9,17 @@ const port = 8082;
 app.use(express.static('public'));
 
 
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.get('/hola-mundo', function (req, res) {
-    res.send('Hola mundo en su respectiva ruta')
-})
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname + '/public/generic.html')
+});
+app.get('/elements', function (req, res) {
+    res.sendFile(__dirname + '/public/elements.html')
+});
 
 app.get('*', function (req, res) {
-    res.send('404 | Page not found')
-})
+    res.sendFile(__dirname + '/public/404.html');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
-
-//app.listen(8082)
+});
