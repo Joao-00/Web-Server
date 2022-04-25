7459eeab848b95b2f3bf289251c5e3aea407a444
Joao Ormeño <joao23.1996@gmail.com>
2022-04-25 19:42:35 -0400

    modificaciones app.js| agrega hbs - error home

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-//paquete utilizado
+//paquetes utilizado
 //https://www.npmjs.com/package/express
 //https://expressjs.com/
+
+//https://www.npmjs.com/package/handlebars
+
 const express = require('express')
 const app = express()
 const port = 8082;
 
+//TODO: require('hbs')
+app.set('view engine', 'hbs');
+
 //para servir contenido estatico
 app.use(express.static('public'));
 
+
+app.get('/', (req, res) =>{
+  res.render('home');
+});
 
 app.get('/generic', function (req, res) {
     res.sendFile(__dirname + '/public/generic.html')
@@ -15,6 +25,8 @@ app.get('/generic', function (req, res) {
 app.get('/elements', function (req, res) {
     res.sendFile(__dirname + '/public/elements.html')
 });
+
+
 
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/404.html');
